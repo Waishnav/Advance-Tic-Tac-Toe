ac044c8eb138087bb79994a226f814a63d110773
Waishnav <waishnavdeore@gmail.com>
2022-08-25 23:27:15 +0530

    made mobile responsive

--- src/main/left-side-bar/leftSideBar.js
+++ src/main/left-side-bar/leftSideBar.js
@@ -3,19 +3,21 @@ import './style.css'
 const LeftSideBar = ({select,frequency}) => {
     return (
         <div className="left-side-bar">
-             <h1>You</h1>
-                 <div className='left' id='left-1' onClick={select}>
+            <h2>Your Friend</h2>
+            <div className="left-side">
+                    <div className='left' id='left-1' onClick={select}>
                         <div className='freq'>{2 - frequency['left-1']}x</div>
-                 </div>
-                 <div className='left' id='left-2' onClick={select}>
+                    </div>
+                    <div className='left' id='left-2' onClick={select}>
                         <div className='freq'>{2 - frequency['left-2']}x</div>
-                 </div>
-                 <div className='left' id='left-3' onClick={select}>
+                    </div>
+                    <div className='left' id='left-3' onClick={select}>
                         <div className='freq'>{2 - frequency['left-3']}x</div>
-                 </div>
-                 <div className='left' id='left-4' onClick={select}>
+                    </div>
+                    <div className='left' id='left-4' onClick={select}>
                         <div className='freq'>{2 - frequency['left-4']}x</div>
-                 </div>
+                    </div>
+            </div>
         </div>
     )
 }
--- src/main/right-side-bar/rightSideBar.js
+++ src/main/right-side-bar/rightSideBar.js
@@ -3,18 +3,20 @@ import './style.css'
 const RightSideBar = ({select,frequency}) => {
     return(
         <div className="right-side-bar">
-            <h1>Your Friend</h1>
-            <div className='right' id='right-1' onClick={select}>
-                <div className='freq'>{2 - frequency['right-1']}x</div>
-            </div>
-            <div className='right' id='right-2' onClick={select}>
-                <div className='freq'>{2 - frequency['right-2']}x</div>
-            </div>
-            <div className='right' id='right-3' onClick={select}>
-                <div className='freq'>{2 - frequency['right-3']}x</div>
-            </div>
-            <div className='right' id='right-4' onClick={select}>
-                <div className='freq'>{2 - frequency['right-4']}x</div>
+            <h2>You</h2>
+            <div className="right-side">
+                    <div className='right' id='right-1' onClick={select}>
+                        <div className='freq'>{2 - frequency['right-1']}x</div>
+                    </div>
+                    <div className='right' id='right-2' onClick={select}>
+                        <div className='freq'>{2 - frequency['right-2']}x</div>
+                    </div>
+                    <div className='right' id='right-3' onClick={select}>
+                        <div className='freq'>{2 - frequency['right-3']}x</div>
+                    </div>
+                    <div className='right' id='right-4' onClick={select}>
+                        <div className='freq'>{2 - frequency['right-4']}x</div>
+                    </div>
             </div>
         </div>
     )
